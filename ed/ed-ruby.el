b73858354032927c3de847939ebec5ad36ddc89a
Edward Paget <edward@cassetteta.pe>
2014-07-29 16:51:29 -0500

    Use Enhanced Ruby Mode and Properly insert ends

--- ed/ed-ruby.el
+++ ed/ed-ruby.el
@@ -1,26 +1,31 @@
 ;; Ruby mode and customizations
 
-(require-package 'rvm)
+(defun ruby-insert-end ()
+  "Insert \"end\" at point and reindent current line."
+  (interactive)
+  (insert "end")
+  (ruby-indent-line t)
+  (end-of-line)) 
 
-(add-hook 'ruby-mode-hook 'rvm-activate-corresponding-ruby)
+(require-package 'rvm)
+(require-package 'rinari)
+(require-package 'rspec-mode)
+(require-package 'enh-ruby-mode)
+(require-package 'ruby-electric)
 (setenv "JRUBY_OPTS" "--2.0")
 
-(require-package 'rinari)
+(add-hook 'enh-ruby-mode-hook 'rvm-activate-corresponding-ruby)
+(add-hook 'enh-ruby-mode-hook 'rspec-mode)
+(add-hook 'enh-ruby-mode-hook 'ruby-electric-mode)
 
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (defadvice ruby-mode-set-encoding
-              (around ruby-mode-set-encoding-disable activate) nil)))
-
-(require-package 'rspec-mode)
-(add-hook 'ruby-mode-hook 'rspec-mode)
-
-(evil-leader/set-key-for-mode 'ruby-mode
+(evil-leader/set-key-for-mode 'enh-ruby-mode
   "v" 'rspec-verify
   "a" 'rspec-verify-all
   "s" 'rspec-toggle-spec-and-target
   "i" 'rspec-rerun
   "m" 'rspec-verify-matching
   "c" 'rspec-verify-continue)
+
+(evil-define-key 'insert enh-ruby-mode-map (kbd "RET") 'evil-ret-and-indent)
 
 (provide 'ed-ruby)
